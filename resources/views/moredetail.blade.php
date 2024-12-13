3c01ca7d880c28652ad017ab093e19a820ed320f
zakaria oarhim <zakariaouarhim2002@gmail.com>
2024-12-13 16:19:00 +0100

    Initial commit

--- resources/views/moredetail.blade.php
+++ resources/views/moredetail.blade.php
@@ -7,9 +7,33 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    
 
     <!-- Custom CSS -->
-     <link rel="stylesheet" href="{{ asset('css/moredetailstyl.css') }}">
+     <link rel="stylesheet" href="{{ asset('css/moredetailstyle.css') }}">
+
+    <!-- Slick Carousel CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick-theme.css">
+
+    <!-- Include Slick Carousel -->
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.min.js"></script>
+
+    <!-- Include Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- jQuery first -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <!-- Bootstrap 4 JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+
 </head>
 <body>
     @include('header')
@@ -94,7 +118,94 @@
             </div>
         </div>
     </div>
-
+    <section id="featured-books" class="py-5">
+    <div class="container">
+        <h2 class="text-center mb-4">الكتب المميزة</h2>
+        <div id="bookCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-6 col-md-2">
+                            <div class="card">
+                                <img src="{{ asset('images/book1.png') }}" class="card-img-top" alt="Book 1">
+                                <div class="card-body">
+                                    <h5 class="card-title">عنوان الكتاب 1</h5>
+                                    <p class="card-text">وصف مختصر للكتاب 5</p>
+                                    <a href="{{ route('moredetail.page') }}" class="btn btn-primary">تفاصيل أكثر</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-2">
+                            <div class="card">
+                                <img src="{{ asset('images/book2.png') }}" class="card-img-top" alt="Book 2">
+                                <div class="card-body">
+                                    <h5 class="card-title">عنوان الكتاب 2</h5>
+                                    <p class="card-text">وصف مختصر للكتاب 5</p>
+                                    <a href="{{ route('moredetail.page') }}" class="btn btn-primary">تفاصيل أكثر</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-2">
+                            <div class="card">
+                                <img src="{{ asset('images/book3.png') }}" class="card-img-top" alt="Book 3">
+                                <div class="card-body">
+                                    <h5 class="card-title">عنوان الكتاب 3</h5>
+                                    <p class="card-text">وصف مختصر للكتاب 5</p>
+                                    <a href="{{ route('moredetail.page') }}" class="btn btn-primary">تفاصيل أكثر</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-2">
+                            <div class="card">
+                                <img src="{{ asset('images/book4.png') }}" class="card-img-top" alt="Book 4">
+                                <div class="card-body">
+                                    <h5 class="card-title">عنوان الكتاب 4</h5>
+                                    <p class="card-text">وصف مختصر للكتاب 5</p>
+                                    <a href="{{ route('moredetail.page') }}" class="btn btn-primary">تفاصيل أكثر</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-2">
+                            <div class="card">
+                                <img src="{{ asset('images/book5.png') }}" class="card-img-top" alt="Book 5">
+                                <div class="card-body">
+                                    <h5 class="card-title">عنوان الكتاب 5</h5>
+                                    <p class="card-text">وصف مختصر للكتاب 5</p>
+                                    <a href="{{ route('moredetail.page') }}" class="btn btn-primary">تفاصيل أكثر</a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <!-- Repeat similar book cards here -->
+                        <div class="col-6 col-md-2">
+                            <div class="card">
+                                <img src="{{ asset('images/book6.png') }}" class="card-img-top" alt="Book 6">
+                                <div class="card-body">
+                                    <h5 class="card-title">عنوان الكتاب 6</h5>
+                                    <p class="card-text">وصف مختصر للكتاب 5</p>
+                                    <a href="{{ route('moredetail.page') }}" class="btn btn-primary">تفاصيل أكثر</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Controls -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#bookCarousel" data-bs-slide="prev" >
+                <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
+                <span class="visually-hidden">السابق</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#bookCarousel" data-bs-slide="next" >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">التالي</span>
+            </button>
+        </div>
+    </div>
+</section>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="product-page.js"></script>
